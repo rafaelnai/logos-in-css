549480fa5996458207e301f7b18fd87040228f05
Rafael Nai <ramiresnai@gmail.com>
2018-10-06 11:26:08 -0300

    template instagram

---
+++ assets/js/Instagram.js
@@ -0,0 +1,14 @@
+const Instagram = () => {
+
+    const template = () => {
+       return `
+            <div class="instagram">
+                <div class="inner-instagram"></div>
+            </div>
+        `;
+    }
+
+    return {
+        template
+    }
+}
